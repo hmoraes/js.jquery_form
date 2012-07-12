871c1e631ccfe922da97672b9fabdc948db864b0
nilo <nilo@localhost>
2012-07-12 22:42:21 +0200

    add version 2.82 of plugin, add minified variant

--- js/jquery_form/__init__.py
+++ js/jquery_form/__init__.py
@@ -1,14 +1,11 @@
-from fanstatic import Library, Resource
-from js.jquery import jquery
+import fanstatic
+import js.jquery
 
-library = Library('jquery_form', 'resources')
+library = fanstatic.Library('jquery_form', 'resources')
 
-# Define the resources in the library like this.
-# For options and examples, see the fanstatic documentation.
-# resource1 = Resource(library, 'style.css')
-
-jquery_form = Resource(
+jquery_form = fanstatic.Resource(
     library,
     'jquery.form.js',
-    depends=[jquery]
+    minified='jquery.form.min.js',
+    depends=[js.jquery.jquery]
 )
